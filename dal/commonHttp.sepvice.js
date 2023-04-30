9a8f8086163b31ada37e13c7b8f254e0cd0082da
klusovich <mklusovich@datamola.com>
2023-04-30 14:46:30 +0300

    remove cors

--- dal/commonHttp.sepvice.js
+++ dal/commonHttp.sepvice.js
@@ -127,9 +127,6 @@ export class CommonHttpService {
         const uploadResult = await window.fetch(fullPath, {
             method: 'PUT',
             headers: {
-                'Sec-Fetch-Mode': 'no-cors',
-                'Sec-Fetch-Site': 'none',
-                'Authorization': 'Bearer ' + localStorage.getItem('jetToken'),
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
